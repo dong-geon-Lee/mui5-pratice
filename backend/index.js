3ca33980c3714cced6222f423c1d0fe8bcb31a73
dong-geon-Lee <ch9716@naver.com>
2023-09-03 09:10:34 +0900

    mongoDB 데이터베이스 구축

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectedDB = require("./config/database");
 
 const app = express();
 const port = process.env.port || 5000;
 
 dotenv.config();
+connectedDB();
 
 app.use(cors());
 app.use(express.json());
