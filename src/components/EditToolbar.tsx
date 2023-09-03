150faf782121f4d37bf03c07773d6633f7a48ca7
dong-geon-Lee <ch9716@naver.com>
2023-09-03 14:34:26 +0900

    Edit 부분 코드 리팩토링

--- src/components/EditToolbar.tsx
+++ src/components/EditToolbar.tsx
@@ -8,7 +8,6 @@ import {
   GridToolbarContainer,
 } from "@mui/x-data-grid";
 
-import { randomId } from "@mui/x-data-grid-generator";
 import { useState } from "react";
 import { Axios } from "../utils/axiosInstance";
 
@@ -17,40 +16,20 @@ interface EditToolbarProps {
   setRowModesModel: (
     newModel: (oldModel: GridRowModesModel) => GridRowModesModel
   ) => void;
-  rows: [];
-  rowModesModel: any;
 }
 
 const EditToolbar = (props: EditToolbarProps) => {
-  const { setRows, setRowModesModel, rows, rowModesModel } = props;
-  const [userData, setUserData] = useState<any>({
+  const { setRows, setRowModesModel } = props;
+  const [userData] = useState<any>({
     name: "",
     age: "",
   });
 
-  // const [addButtonStatus, setAddButtonStatus] = useState(false);
-
-  // const createRequest = async () => {
-  //   const response = await Axios.post("/api/traders", userData);
-  //   console.log(response.data);
-  // };
-
-  // console.log(rows, rowModesModel);
-
-  // if (rowModesModel && addButtonStatus) {
-  //   console.log("생성 클릭반응!");
-  //   createRequest();
-  //   setAddButtonStatus(false);
-  // }
-  console.log(userData);
-
   const handleClick = async () => {
     const response = await Axios.post("/api/traders", userData);
-    console.log(response.data);
-
     const { _id: id, name, age } = response.data;
 
-    setRows((oldRows) => [...oldRows, { id, name, age, isNew: true }]);
+    setRows((oldRows) => [{ id, name, age, isNew: true }, ...oldRows]);
     setRowModesModel((oldModel) => ({
       ...oldModel,
       [id]: { mode: GridRowModes.Edit, fieldToFocus: "name" },
